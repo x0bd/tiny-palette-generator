7e1f2141a84179629a419ec9d3e6e384ddf635c6
Tinodaishe Tembo <synthmancer@gmail.com>
2023-07-06 15:39:44 +0200

    wip styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 import "./App.css";
 
-function App() {}
+const gallery = <i class="fa-solid fa-images"></i>;
+
+function App() {
+	return (
+		<>
+			<header>
+				<h1>Palegen</h1>
+				<div className="input">
+					<label htmlFor="file">{gallery} Upload Image</label>
+					<input type="file" id="file" hidden />
+				</div>
+			</header>
+		</>
+	);
+}
 
 export default App;
