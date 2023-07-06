c52375ebd8443c1a1bc7c0f7824292c9df4a428f
Tinodaishe Tembo <synthmancer@gmail.com>
2023-07-06 21:53:47 +0200

    wip

--- src/components/DisplayImage.jsx
+++ src/components/DisplayImage.jsx
@@ -1,6 +1,16 @@
 import "./displayImage.css";
+import ListItem from "./ListItem";
 
 export default function DisplayImage({ uploadedImage, colorPalette }) {
+	const toHex = (rgb) => {
+		let hex = Number(rgb).toString(16);
+		if (hex.length < 2) {
+			hex = "0" + hex;
+		}
+
+		return hex;
+	};
+
 	return (
 		<div className="content">
 			<div className="image">
@@ -10,6 +20,24 @@ export default function DisplayImage({ uploadedImage, colorPalette }) {
 					"Please upload an image.."
 				)}
 			</div>
+
+			{colorPalette && (
+				<ul>
+					{colorPalette.map((color, index) => {
+						const rgb = `rgb(${color.join(",")})`;
+						console.log(rgb);
+						const hex =
+							"#" +
+							toHex(color[0]) +
+							toHex(color[1]) +
+							toHex(color[2]);
+						console.log(hex, rgb);
+						return (
+							<ListItem key={index} rgb={rgb} hex={hex} />
+						);
+					})}
+				</ul>
+			)}
 		</div>
 	);
 }
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import "./listItem.css";
 
-const ListItem = () => {
-	return <div>ListItem</div>;
+const copy = <i className="far fa-copy"></i>;
+
+const ListItem = ({ rgb, hex }) => {
+	const [copied, setCopied] = useState(false);
+
+	return (
+		<div className="colorName" style={{ background: rgb }}>
+			<span>
+				{copied ? "Copied" : hex} {copy}
+			</span>
+		</div>
+	);
 };
 
 export default ListItem;
